ff8741ed3dbfa0de7f9bfae966b6cb1343ed1b50
GopikaSanthosh <92243019+GopikaSanthosh@users.noreply.github.com>
2021-10-20 19:36:14 +0530

    Update ProblemsolversvsDevelopers.py

--- task-03/Problem Solvers vs Developers/ProblemsolversvsDevelopers.py
+++ task-03/Problem Solvers vs Developers/ProblemsolversvsDevelopers.py
@@ -1,1 +1,27 @@
-
+n,m=map(int,input().split())
+d1=input().split()
+p1=input().split()
+p=[]
+d=[]
+for i in d1:
+    d.append(int(i))
+for i in p1:
+    p.append(int(i))    
+ds=sorted(d) 
+ps=sorted(p)
+i=0
+j=0
+mp=[]
+c=0
+s=0
+j=0
+for i in range(m):
+    if ps[i]>ds[j]:
+        s+=ps[i]
+        j+=1
+        c+=1
+    if c==n:
+        print('YES')
+        print(s)
+        exit()
+print('NO')
