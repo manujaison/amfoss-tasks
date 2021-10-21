3d8e176a1540ed1d1a10ee7c74daeda23c83f2b2
GopikaSanthosh <92243019+GopikaSanthosh@users.noreply.github.com>
2021-10-21 20:08:55 +0530

    Update Aver_Max.py

--- task-03/Aver_Max/Aver_Max.py
+++ task-03/Aver_Max/Aver_Max.py
@@ -5,7 +5,7 @@ for i in range(t):
     s=input().split(' ')
     for i in s:
         s1.append(float(i))
-    if k>0 and  min(s1)<0:
+    if k>0 and  min(s1)<0 and (-1*min(s1))>max(s1):
         print(-1*min(s1),end=' ')
     else:
         print(max(s1))
