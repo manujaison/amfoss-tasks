a9ca9bfbf3273e8cc4f04733e17def4af49c2756
GopikaSanthosh <92243019+GopikaSanthosh@users.noreply.github.com>
2021-11-10 17:09:58 +0530

    Create SandeshAndStrings.py

---
+++ task-03/Sandesh and Strings/SandeshAndStrings.py
@@ -0,0 +1,28 @@
+n=int(input())
+s=input()
+m=''
+f=''
+for i in range (n):
+    if s.count(s[i])>1 and s[i] not in f:
+        f+=s[i]
+    elif s.count(s[i])>2 and f.count(s[i])<s.count(s[i])//2:  
+        f+=s[i]
+    elif s.count(s[i])%2!=0 and len(m)<1 and n%2==0:
+        m+=s[i]
+    elif s.count(s[i])%2!=0 and s[i] not in f and n%2!=0 and s.count(s[n//2])%2==0:
+        m+=s[i]
+if n%2!=0 and s.count(s[n//2])%2!=0:
+    m+=s[n//2]
+sh=''    
+sh+=f[::-1]  
+fs=f+m+sh
+if len(fs)==0:
+    print(0)
+    print(s[0])
+else:    
+    print(len(fs))
+    print(fs)
+    
+    
+        
+
